280509b0668c086a2eb6d59105428e0d061181d0
sepour <31295509+spour@users.noreply.github.com>
2023-01-02 11:36:48 -0500

    Update cnn_dist.py

--- cnn_dist.py
+++ cnn_dist.py
@@ -1,69 +1,83 @@
 from tensorflow import keras
-from tfomics import layers, utils
+from . import custom_layers, utility_functions
 
 
-def model(activation='relu'):
+def model(custom_activation='relu'):
+    """
+    Creates a CNN model for binary classification.
+    
+    Parameters
+    ----------
+    activation: str, optional
+        Activation function to use for the layers. Default is 'relu'.
+        
+    Returns
+    -------
+    model: Keras model
+        Compiled Keras model with the specified layers and activations.
+    """
       
-    # input layer
+    # inputs
     inputs = keras.layers.Input(shape=(200,4))
-    activation = utils.activation_fn(activation)
+    custom_activation = utility_functions.custom_activation_fn(custom_activation)
 
-    # block 1
-    nn = layers.conv_layer(inputs,
-                           num_filters=24, 
-                           kernel_size=19, 
-                           padding='same', 
-                           activation=activation, 
-                           dropout=0.1,
-                           l2=1e-6, 
-                           bn=True)
+    # layer 1
+    x = custom_layers.conv_layer(inputs,
+                              num_filters=24, 
+                              kernel_size=19, 
+                              padding='same', 
+                              activation=custom_activation, 
+                              dropout=0.1,
+                              l2_reg=1e-6, 
+                              batch_norm=True)
 
     # layer 2
-    nn = layers.conv_layer(nn,
-                           num_filters=32, 
-                           kernel_size=7, 
-                           padding='same', 
-                           activation='relu', 
-                           dropout=0.2,
-                           l2=1e-6, 
-                           bn=True)
-    nn = keras.layers.MaxPool1D(pool_size=4)(nn)
+    x = custom_layers.conv_layer(x,
+                              num_filters=32, 
+                              kernel_size=7, 
+                              padding='same', 
+                              activation='relu', 
+                              dropout=0.2,
+                              l2_reg=1e-6, 
+                              batch_norm=True)
+    x = keras.layers.MaxPool1D(pool_size=4)(x)
+
 
     # layer 3
-    nn = layers.conv_layer(nn,
-                           num_filters=48, 
-                           kernel_size=7, 
-                           padding='valid', 
-                           activation='relu', 
-                           dropout=0.3,
-                           l2=1e-6, 
-                           bn=True)
-    nn = keras.layers.MaxPool1D(pool_size=4)(nn)
+    x = custom_layers.conv_layer(x,
+                              num_filters=48, 
+                              kernel_size=7, 
+                              padding='valid', 
+                              activation='relu', 
+                              dropout=0.3,
+                              l2_reg=1e-6, 
+                              batch_norm=True)
+    x = keras.layers.MaxPool1D(pool_size=4)(x)
 
     # layer 4
-    nn = layers.conv_layer(nn,
-                           num_filters=64, 
-                           kernel_size=3, 
-                           padding='valid', 
-                           activation='relu', 
-                           dropout=0.4,
-                           l2=1e-6, 
-                           bn=True)
-    nn = keras.layers.MaxPool1D(pool_size=3, 
+    x = custom_layers.conv_layer(x,
+                                num_filters=64, 
+                                kernel_size=3, 
+                                padding='valid', 
+                                activation='relu', 
+                                dropout=0.4,
+                                l2_reg=1e-6, 
+                                batch_norm=True)
+    x = keras.layers.MaxPool1D(pool_size=3, 
                                 strides=3, 
                                 padding='same'
-                                )(nn)
+                                )(x)
 
     # layer 5
-    nn = keras.layers.Flatten()(nn)
-    nn = layers.dense_layer(nn, num_units=96, activation='relu', 
-                            dropout=0.5, l2=1e-6, bn=True)
+    x = keras.layers.Flatten()(x)
+    x = custom_layers.dense_layer(x, num_units=96, activation='relu', 
+                            dropout=0.5, l2_reg=1e-6, batch_norm=True)
 
-    # Output layer 
-    logits = keras.layers.Dense(1, activation='linear', use_bias=True)(nn)
+
+    logits = keras.layers.Dense(1, activation='linear', bias=True)(x)
     outputs = keras.layers.Activation('sigmoid')(logits)
         
-    # compile model
+    #put together
     model = keras.Model(inputs=inputs, outputs=outputs)
 
     return model
