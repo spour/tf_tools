096268b2b1a0c68ae0ac2d05945a8d421094c983
sepour <31295509+spour@users.noreply.github.com>
2023-01-02 10:59:38 -0500

    Update basset.py

--- basset.py
+++ basset.py
@@ -1,59 +1,68 @@
-from tensorflow import keras
-from tfomics import layers, utils
+import tensorflow
+from . import custom_layers, utility_functions
 
 
-def model(activation='relu'):
+def build_model(act_fn: str ='relu'):
+    """
+    Builds a 1D convolutional neural network model for binary classification for genomics like Kelley's Basset model.
+
+    Parameters:
+    - act_fn (str): Activation function to use in the layers. Can be 'relu' or 'tanh'. Default is 'relu'.
+   
+    Returns:
+    - A Keras model.
+    """
 
     # input layer
-    inputs = keras.layers.Input(shape=(600,4))
+    inputs = tf.keras.layers.Input(shape=(600,4))
     
-    activation = utils.activation_fn(activation)
+    act_fn = utility_functions.get_activation(act_fn)
     
     # layer 1
-    nn = layers.conv_layer(inputs,
+    x = custom_layers.convolutional(inputs,
                            num_filters=300, 
                            kernel_size=19,   # 192
                            padding='same', 
                            activation=activation, 
                            dropout=0.2,
-                           l2=1e-6, 
-                           bn=True)
-    nn = keras.layers.MaxPool1D(pool_size=3)(nn)
+                           l2_reg=1e-6, 
+                           batch_norm=True)
+    x = tf.keras.layers.MaxPool1D(pool_size=3)(x)
 
     # layer 2
-    nn = layers.conv_layer(nn,
+    x = custom_layers.convolutional(x,
                            num_filters=200, 
                            kernel_size=11,  # 56
                            padding='valid', 
                            activation='relu', 
                            dropout=0.2,
-                           l2=1e-6, 
-                           bn=True)
-    nn = keras.layers.MaxPool1D(pool_size=4)(nn)
+                           l2_reg=1e-6, 
+                           batch_norm=True)
+    x = tf.keras.layers.MaxPool1D(pool_size=4)(x)
 
     # layer 3
-    nn = layers.conv_layer(nn,
+    x = custom_layers.convolutional(x,
                            num_filters=200, 
                            kernel_size=7,  # 56
                            padding='valid', 
                            activation='relu', 
                            dropout=0.2,
-                           l2=1e-6, 
-                           bn=True)
-    nn = keras.layers.MaxPool1D(pool_size=4)(nn)
+                           l2_reg=1e-6, 
+                           batch_norm=True)
+    x = tf.keras.layers.MaxPool1D(pool_size=4)(x)
 
     # layer 4
-    nn = keras.layers.Flatten()(nn)
-    nn = layers.dense_layer(nn, num_units=1000, activation='relu', dropout=0.5, l2=1e-6, bn=True)
+    x = tf.keras.layers.Flatten()(x)
+    x = custom_layers.convolutional.dense_layer(x, num_units=1000, activation='relu', dropout=0.5, l2_reg=1e-6, batch_norm=True)
 
     # layer 5
-    nn = layers.dense_layer(nn, num_units=1000, activation='relu', dropout=0.5, l2=1e-6, bn=True)
+    x = custom_layers.dense_layer(x, num_units=1000, activation='relu', dropout=0.5, l2_reg=1e-6, batch_norm=True)
 
     # Output layer 
-    logits = keras.layers.Dense(164, activation='linear', use_bias=True)(nn)
-    outputs = keras.layers.Activation('sigmoid')(logits)
+    logits = tf.keras.layers.Dense(164, activation='linear', use_bias=True)(x)
+    outputs = tf.keras.layers.Activation('sigmoid')(logits)
         
-    model = keras.Model(inputs=inputs, outputs=outputs)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
     return model
 
